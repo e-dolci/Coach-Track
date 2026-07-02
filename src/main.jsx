88853f25b8f7b3824e0bbd6beea3d65a182b5888
Emma Dolci <esue3752@gmail.com>
2026-07-01 22:43:58 -0400

    Fix basename placement on BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import App from './App';
 import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Coach-Track/">
       <AuthProvider>
         <App />
         <Toaster position="top-right" toastOptions={{ duration: 3000, style: { borderRadius: '12px', fontSize: '14px' } }} />
       </AuthProvider>
-    </BrowserRouter basename="/Coach-Track/">
+    </BrowserRouter>
   </React.StrictMode>
 );
